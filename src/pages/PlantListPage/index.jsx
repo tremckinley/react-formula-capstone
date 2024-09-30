7a4ef563f2664cf0b9054f56acabe5c71d7ae852
Tremaine McKinley <49395726+tremckinley@users.noreply.github.com>
2024-09-30 02:55:08 +0000

    added loading spinner

--- src/pages/PlantListPage/index.jsx
+++ src/pages/PlantListPage/index.jsx
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import NavBar from "shared-components/NavBar";
 import SignOutRedirect from "shared-components/SignOutRedirect";
 import * as plantService from "services/plant";
 
 export default function PlantListPage() {
-    
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         (async () => {
+            setLoading(true)
             const response = await plantService.getPlants();
             console.log(response.status)
             const plantList = await response.json()
+            setLoading(false)
             console.log(plantList);
         })()
     }, [])
@@ -17,7 +20,7 @@ export default function PlantListPage() {
     return (
         <SignOutRedirect>
             <NavBar />
-            
+            {loading && <div className="h-screen flex justify-center items-center"><i className="fa fa-sun animate-spin text-5xl text-emerald-500"></i></div>}
         </SignOutRedirect>
     )
 
